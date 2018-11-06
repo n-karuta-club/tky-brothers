fa8b33aa5c816976a0a99726cdd3a068d8aa022c
high-shore-built <s16t241@stu.kagawa-u.ac.jp>
2018-11-06 18:15:59 +0900

    リザルト画面への自動遷移を可能にした。

--- src/main/java/config/WindowConfig.java
+++ src/main/java/config/WindowConfig.java
@@ -5,5 +5,5 @@ public class WindowConfig {
     public static final int ySize = 600;
     public static final int gravity = 1;
     public static final int sleep = 50;
-    public static final int gameTime = 180;
+    public static final int gameTime = 5;
 }
