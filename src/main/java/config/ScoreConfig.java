87646c44cea5b983ab7bb65fd1ce859af19515fa
shiotomo <t.shiota16@gmail.com>
2018-11-12 02:52:12 +0900

    リザルト画面に移行してからすぐにボタンを押してしまうことの内容に3秒後から入力を受け付けるようにした

--- src/main/java/config/ScoreConfig.java
+++ src/main/java/config/ScoreConfig.java
@@ -1,63 +1,6 @@
 package config;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import lombok.val;
-
 public class ScoreConfig {
     public static final int enemyBreakPoint = 100;
-
-    public static String httpPost(String postUrl, String JSON) {
-        HttpURLConnection httpURLConnection = null;
-        val result = new StringBuffer();
-
-        try {
-            val url = new URL(postUrl);
-            httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.setRequestProperty("Accept-Language", "jp");
-            httpURLConnection.setRequestProperty("Content-Type", "application/JSON; charset=utf8");
-            httpURLConnection.setRequestProperty("Content-Length", String.valueOf(JSON.length()));
-            httpURLConnection.setDoOutput(true);
-            val outputStreamWriter = new OutputStreamWriter(httpURLConnection.getOutputStream());
-            outputStreamWriter.write(JSON);
-            outputStreamWriter.flush();
-            httpURLConnection.connect();
-
-            val status = httpURLConnection.getResponseCode();
-            // リクエストの送信に成功したか判定
-            if (status == HttpURLConnection.HTTP_OK) {
-                // 通信に成功した
-                // テキストを取得する
-                //  val inputStream = httpURLConnection.getInputStream();
-                //  val encoding = httpURLConnection.getContentEncoding() == null ? httpURLConnection.getContentEncoding() : "UTF-8";
-                //  val inputStreamReader = new InputStreamReader(inputStream, encoding);
-                //  val bufferedReader = new BufferedReader(inputStreamReader);
-                //  String line = null;
-                //   // 1行ずつテキストを読み込む
-                //  while ((line = bufferedReader.readLine()) != null) {
-                //      result.append(line);
-                //  }
-                //  bufferedReader.close();
-                //  inputStreamReader.close();
-                //  inputStream.close();
-                System.out.println("success!!");
-                System.out.println(status);
-            } else {
-                System.out.println("failed...");
-                System.out.println(status);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (httpURLConnection != null) {
-                httpURLConnection.disconnect();
-            }
-        }
-
-        return result.toString();
-    }
+    public static final String serverApiUrl = "http://localhost:8080/api/score/";
 }
--- src/main/java/game/ResultGame.java
+++ src/main/java/game/ResultGame.java
@@ -1,6 +1,7 @@
 package game;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -9,6 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import block.Score;
+import block.Timer;
 import config.WindowConfig;
 import lombok.val;
 import service.EnemyService;
@@ -17,21 +19,25 @@ import service.FloorService;
 import service.PlayerService;
 import service.ScoreService;
 
-public class ResultGame extends JPanel implements KeyListener {
+public class ResultGame extends JPanel implements Runnable, KeyListener {
     private boolean isThisWindow = false;
     private GameWindow gameWindow;
+    private Timer timer;
+    private Thread thread = null;
 
     public ResultGame(GameWindow gameWindow, Score score) {
         setPreferredSize(new Dimension(WindowConfig.xSize, WindowConfig.ySize));
         val startButton = new JButton("タイトルへ戻る");
         val scoreLabel = new JLabel(String.valueOf(score.getPoint()));
-        isThisWindow = true;
         this.gameWindow = gameWindow;
+        timer = new Timer(3, 0, WindowConfig.xSize - 50, WindowConfig.ySize - 50);
+
         PlayerService.resetList();
         EnemyService.resetList();
         FireService.resetList();
         FloorService.resetList();
 
+        // スコアをサーバに送る
         ScoreService.postScore(score.getPoint());
 
         startButton.addActionListener(event -> {
@@ -42,7 +48,50 @@ public class ResultGame extends JPanel implements KeyListener {
         add(startButton);
         add(scoreLabel);
         this.gameWindow.addKeyListener(this);
-        isThisWindow = true;
+        startThread();
+    }
+
+    /**
+     * プログラム実行時のThreadを取得し、Threadをスタートする。
+     */
+    public void startThread() {
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    /**
+     * プログラムを実行する。
+     */
+    @Override
+    public void run() {
+        Thread currentThread = Thread.currentThread();
+
+        while (thread == currentThread) {
+            timer.status();
+            if (timer.stateNowTime()) {
+                isThisWindow = true;
+                break;
+            }
+            repaint();
+            try {
+                Thread.sleep(WindowConfig.sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * タイマー表示画面
+     */
+    @Override
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        if (timer.getNowTime() > 0) {
+            timer.print(graphics);
+        }
     }
 
     /**
@@ -54,8 +103,8 @@ public class ResultGame extends JPanel implements KeyListener {
             return;
         }
         switch (e.getKeyCode()) {
-        case KeyEvent.VK_S:
         case KeyEvent.VK_DOWN:
+        case KeyEvent.VK_S:
             isThisWindow = false;
             gameWindow.change(new StartGame(gameWindow));
         }
@@ -65,13 +114,11 @@ public class ResultGame extends JPanel implements KeyListener {
      * Keyをtypeした時
      */
     @Override
-    public void keyTyped(KeyEvent e) {
-    }
+    public void keyTyped(KeyEvent e) {}
 
     /**
      * Keyを話した時
      */
     @Override
-    public void keyReleased(KeyEvent e) {
-    }
+    public void keyReleased(KeyEvent e) {}
 }
